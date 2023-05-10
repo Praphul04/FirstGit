05051945731f409758fb3bb922aef4f30be88133
Praphul04 <praphulthakur6@gmail.com>
2023-05-10 10:59:54 +0530

    First commit Message

--- demo.js
+++ demo.js
@@ -14,13 +14,26 @@
 // console.log(document.images);
 
 // console.log(document.getElementById("header-title"));
-var headerTitle = document.getElementById("header-title");
-// var header = document.getElementById("main-header");
-// console.log(headerTitle);
-// headerTitle.textContent = "hello";
-// headerTitle.innerText = "GoodBye";
-// headerTitle.innerHTML = "<h3>Hello</h3>";
-// headerTitle.style.border = 'solid 3px #000';
-var Main = document.getElementById("main");
-// console.log(Main);
-Main.style.fontWeight  = 'bold';
+// var headerTitle = document.getElementById("header-title");
+// // var header = document.getElementById("main-header");
+// // console.log(headerTitle);
+// // headerTitle.textContent = "hello";
+// // headerTitle.innerText = "GoodBye";
+// // headerTitle.innerHTML = "<h3>Hello</h3>";
+// // headerTitle.style.border = 'solid 3px #000';
+// var Main = document.getElementById("main");
+// // console.log(Main);
+// Main.style.fontWeight  = 'bold';
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'hello2';
+// items[1].style.fontWeight = 'bold';
+// items[2].style.backgroundColor = 'green';
+
+
+// items.style.backgroundColor = 'yellow';
+for(var i = 0; i< items.length; i++){
+    items[i].style.fontWeight= 'bold';
+    items[i].style.backgroundColor='yellow';
+}
