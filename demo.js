94d50821e9516634a0843fa8c821994add544f36
Praphul04 <praphulthakur6@gmail.com>
2023-05-10 10:28:59 +0530

    First commit Message

--- demo.js
+++ demo.js
@@ -1,1 +1,26 @@
-console.log("Hello Git")
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = 133;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent= "Hello";
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+// console.log(document.getElementById("header-title"));
+var headerTitle = document.getElementById("header-title");
+// var header = document.getElementById("main-header");
+// console.log(headerTitle);
+// headerTitle.textContent = "hello";
+// headerTitle.innerText = "GoodBye";
+// headerTitle.innerHTML = "<h3>Hello</h3>";
+// headerTitle.style.border = 'solid 3px #000';
+var Main = document.getElementById("main");
+// console.log(Main);
+Main.style.fontWeight  = 'bold';
